39472ed0004fb53fd9ca38556f00b251966fce94
JieJeanL <942311608@qq.com>
2015-12-06 20:57:30 -0500

    Update HWK6_prinsij.cpp
    I am confused about stof("abc"). What does that mean?

--- HWK6_prinsij.cpp
+++ HWK6_prinsij.cpp
@@ -1,7 +1,7 @@
 /*
  * Name: Ian Prins, Jie Luo, Eric Chaput, Jiamin Luo
- * MacID: prinsij, louj3, chaputem, luoj1
- * Student Number: 001419316, 
+ * MacID: prinsij, luoj3, chaputem, luoj1
+ * Student Number: 001419316, 001416119
  * Description: This file implements the class Add, representing an addition 
  * 				arithmatic expression
  */
